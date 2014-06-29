ef62a3cd6e58c1bd581620a1dbf5529669ab4ce6
Metida <me.metida@gmail.com>
2014-06-30 01:53:42 +0400

    Update ffe.h
    multiplication added

--- ffe.h
+++ ffe.h
@@ -7,22 +7,34 @@ private:
   int value;
   
 public:
- 	FFE<N>::FFE(int value){
-	    	FFE<N>::value = value%N;
-	 }
+  FFE<N>::FFE(int value){
+    FFE<N>::value = value%N;
+  }
 	 
   FFE<N>& FFE<N>::operator+=(const FFE<N>& b){
-		  this->value += b.value%N;
-		  if (this->value > N)
-			   this->value = this->value %N;
-		  return *this;
-	 }
+    this->value += b.value%N;
+    if (this->value > N)
+      this->value = this->value %N;
+		return *this;
+	}
+	
+	FFE<N>& FFE<N>::operator*=(const FFE<N>& b){
+		this->value *= b.value%N;
+		this->value = this->value%N;
+		return *this;
+	}
 };
 
 template<long N>
 FFE<N> operator+(const FFE<N>& a, const FFE<N>& b){
  	FFE<N> *temp = new FFE<N>(a);
 	 return temp += b;
+}
+
+template<long N>
+FFE<N> operator*(const FFE<N>& a, const FFE<N>& b){
+	FFE<N> *temp = new FFE<N>(a);
+	return temp *= b;
 }
  
 #endif /* FFE_H */
