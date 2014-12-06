8d3db3ee212919c10d523575c44f2d8ebc40e6e5
Artem Pelenitsyn <ulysses4ever@gmail.com>
2014-12-06 16:53:46 +0300

    More structuring in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
-#include "ffe.h"
-#include "is_prime.h"
+//#include "ffe.h"
+//#include "is_prime.h"
+#include "poly.h"
+
 using namespace std;
 
-/*
-	const FFE<N> mult(const FFE<N>&a, const FFE<N>& b) {
-		if (!value || !b.value) return FFE<N>(0);
-		if (value&1) return FFE<N>((b.value + ::mult<N>(value-1, b.value))%N);
-		return FFE<N>((::mult<N>(value/2, b.value)*2)%N);
-	}
-*/
-
 int main(){
-	const long N = 7;
+    // Test FFE
+/*	const long N = 7;
 	cout << "N=" << N << endl;
 	FFE<N> *a = new FFE<N>(5);
 	FFE<N> *b = new FFE<N>(4);
 	*a += *b;
 	const FFE<N> *c = FFE<N>::ONE();
+
+    // Test is_prime
 	bool j = is_prime<11>::value;
+*/
+
+    // Test sum
+
+
+    typedef sum<3, Poly<3, 2>, Poly<3, 2>>::type s;
+    cout << s::elements[0] << endl;
+
 }
