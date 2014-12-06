bcd89528aaa9a67e8247cd27d4afe8e736a4ce53
Artem Pelenitsyn <ulysses4ever@gmail.com>
2014-12-06 16:54:58 +0300

    Create basic definition for sum (of Poly's) metafunction
    1) general template
    
    2) partial specialization with Poly's of any length
    
    2) partial specializaation for 0-deg Poly's

--- poly.h
+++ poly.h
@@ -1,13 +1,43 @@
 #ifndef POLY_H
 #define POLY_H
 
+#include "ffe.h"
+
 template <int N, int ... Nums>
 struct Poly {
 	static FFE<N> elements[sizeof ...(Nums)];
 	enum { elements_count = sizeof ...(Nums) };
 };
 
-template<int N, int ... Nums>
+template<int N, int... Nums>
 FFE<N> Poly<N, Nums ...>::elements[] = { Nums ... };
 
+
+
+template<int N, typename P1, typename P2>
+struct sum {
+};
+
+/*template<int N, int ... Nums>
+struct sum<N, Poly<N, int ... Nums>, > {
+};*/
+
+
+template<int N, int C1, int C2, int... Nums1, int... Nums2>
+struct sum<N, Poly<N,C1, Nums1 ...>,
+              Poly<N,C2, Nums2 ...>> {
+    typedef Poly<N, C1+C2>// ++ ???
+        // typename sum<N,
+            //Poly<N,Nums1>, Poly<N,Nums2>>::type
+            type;
+};
+/*
+
+template<int N, int C1, int C2>
+struct sum<N, Poly<N,C1>, Poly<N, C2>> {
+
+    typedef Poly<N, C1 + C2> type;
+
+};
+*/
 #endif
