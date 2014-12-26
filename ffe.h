d573353249d159039210869fee9d1b05c5dbdd55
Metida <me.metida@gmail.com>
2014-12-26 13:58:22 +0300

    try to fix !=, <<

--- ffe.h
+++ ffe.h
@@ -12,37 +12,43 @@ private:
   int value;
 
 public:
-  FFE(int value){
-    static_assert(is_prime_func(N), "Field order must be a prime number");
-    FFE<N>::value = value%N;
-  }
+	FFE(int value){
+	static_assert(is_prime_func(N), "Field order must be a prime number");
+		FFE<N>::value = value%N;
+	}
 
-  static const FFE<N>* ONE(){
-    return new FFE<N>(1);
-  }
+	static const FFE<N>* ONE(){
+		return new FFE<N>(1);
+	}
 
-  static const FFE<N>* ZERO(){
-  	return new FFE<N>(0);
-  }
+	static const FFE<N>* ZERO(){
+		return new FFE<N>(0);
+	}
 
-  FFE<N>& operator+=(const FFE<N>& b){
-    this->value += b.value%N;
-    if (this->value > N)
-      this->value = this->value %N;
-    return *this;
-  }
+	FFE<N>& operator+=(const FFE<N>& b){
+		this->value += b.value%N;
+		if (this->value > N)
+			this->value = this->value %N;
+		return *this;
+	}
 
-  FFE<N>& operator*=(const FFE<N>& b){
-    this->value *= b.value%N;
-    this->value = this->value%N;
-    return *this;
-  }
+	FFE<N>& operator*=(const FFE<N>& b){
+		this->value *= b.value%N;
+		this->value = this->value%N;
+		return *this;
+	}
+ 
+	template<long M>
+	friend
+	ostream & operator<<(ostream& os, const FFE<M>& ffe){
+		return os << ffe.value;
+	}
 
-    template<long M>
-    friend
-    ostream & operator<<(ostream& os, const FFE<M>& ffe){
-        return os << ffe.value;
-    }
+//	template<long M>
+//	friend
+//		constexpr bool operator!=(const FFE<M>& a, const int b) {
+//		return a.value != b;
+//	}
 };
 
 
@@ -58,6 +64,9 @@ FFE<N> operator*(const FFE<N>& a, const FFE<N>& b){
 	return temp *= b;
 }
 
-#endif /* FFE_H */
+template<long N>
+constexpr bool operator!=(const FFE<N>& a, const FFE<N>& b) {
+	return a.value != b.value;
+}
 
-//http://en.cppreference.com/w/cpp/types/enable_if
+#endif /* FFE_H */
--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
-//#include "ffe.h"
-//#include "is_prime.h"
+#include "ffe.h"
 #include "poly.h"
+#include "is_prime.h"
 
 using namespace std;
 
 int main(){
     // Test FFE
-/*	const long N = 7;
-	cout << "N=" << N << endl;
-	FFE<N> *a = new FFE<N>(5);
-	FFE<N> *b = new FFE<N>(4);
-	*a += *b;
+	const long N = 7;
+	FFE<N> a(5);
+	FFE<N> b(4);
+	static_assert(a != 2, "Test for sum of two finite field elements failed");
 	const FFE<N> *c = FFE<N>::ONE();
 
+/*  // Вельшенбах. Shoup: book algorithms in number theory, algebra, inmplementation: NTL
     // Test is_prime
 	bool j = is_prime<11>::value;
+
 */
 
     // Test sum
-
-
-    typedef sum<3, Poly<3, 2>, Poly<3, 2>>::type s;
-    cout << s::elements[0] << endl;
+    typedef sum<3, Poly<3, 2, 1>, Poly<3, 2>>::type s;
+    cout << s::elements[1] << endl;
 
 }
