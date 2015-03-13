801881414e66bc693368cade76f0920e0c8fe876
Anastasia Myshko <me.metida@gmail.com>
2015-03-14 01:08:49 +0300

    Fix formatting.

--- poly.h
+++ poly.h
@@ -5,8 +5,8 @@
 
 template <int N, int ... Nums>
 struct Poly {
-	static FFE<N> elements[sizeof ...(Nums)];
-	enum { elements_count = sizeof ...(Nums) };
+    static FFE<N> elements[sizeof ...(Nums)];
+    enum { elements_count = sizeof ...(Nums) };
 };
 
 template<int N, int... Nums>
@@ -18,7 +18,7 @@ struct concat {
 
 template<int N, int... Nums1, int... Nums2>
 struct concat<N, Poly<N, Nums1 ...>, 
-				 Poly<N, Nums2 ...>> {
+                 Poly<N, Nums2 ...>> {
 	typedef Poly<N, Nums1 ..., Nums2 ...> type;
 };
 
@@ -29,10 +29,9 @@ struct sum {
 };
 
 template<int N, int C1, int C2, int... Nums1, int... Nums2>
-struct sum<N, Poly<N, C1, Nums1 ...>,
-              Poly<N, C2, Nums2 ...>> {
+struct sum<N, Poly<N, C1, Nums1 ...>, Poly<N, C2, Nums2 ...>> {
     typedef typename concat<N, 
-		                    Poly<N, C1+C2>, 
+                            Poly<N, C1+C2>, 
                             typename sum<N, 
 							             Poly<N, Nums1 ...>, 
                                          Poly<N, Nums2 ...>>::type
@@ -46,12 +45,12 @@ struct sum<N, Poly<N, C1>, Poly<N, C2>> {
 
 template <int N, int C1, int C2, int... Nums2>
 struct sum<N, Poly<N, C1>, Poly<N, C2, Nums2...>> {
-	typedef typename concat<N, Poly<N, C1 + C2>, Poly<N, Nums2...>>::type type;
+    typedef typename concat<N, Poly<N, C1 + C2>, Poly<N, Nums2...>>::type type;
 };
 
 template <int N, int C1, int C2, int... Nums1>
 struct sum<N, Poly<N, C1, Nums1...>, Poly<N, C2>> {
-	typedef typename concat<N, Poly<N, C1 + C2>, Poly<N, Nums1...>>::type type;
+    typedef typename concat<N, Poly<N, C1 + C2>, Poly<N, Nums1...>>::type type;
 };
 
 //mult
@@ -62,26 +61,26 @@ struct mult {
 
 template <int N, int C1, int C2, int... Nums1>
 struct mult<N, Poly<N, C1, Nums1...>, Poly<N, C2>> {
-	typedef typename concat <N,
-							Poly<N, C1 * C2>,
-							typename mult<N, Poly<N, Nums1...>, Poly<N, C2>>::type
-							>::type type;
+    typedef typename concat <N,
+                            Poly<N, C1 * C2>,
+                            typename mult<N, Poly<N, Nums1...>, Poly<N, C2>>::type
+                            >::type type;
 };
 
 template <int N, int C1, int C2>
 struct mult<N, Poly<N, C1>, Poly<N, C2>> {
-	typedef Poly<N, C1 * C2> type;
+    typedef Poly<N, C1 * C2> type;
 };
 
 template <int N, int C1, int... Nums1, int C2, int... Nums2>
 struct mult<N, Poly<N, C1, Nums1...>, Poly<N, C2, Nums2...>> {
-	typedef typename sum <N,
-						  Poly<N, C1, Nums1...>,
-						  typename concat<N,
-										  Poly<N, 0>,
-										  typename mult<N, Poly<N, C1, Nums1...>, Poly<N, Nums2...>>::type
-										  >::type
-						 >::type type;
+    typedef typename sum <N,
+                          Poly<N, C1, Nums1...>,
+                          typename concat<N,
+                                          Poly<N, 0>,
+                                          typename mult<N, Poly<N, C1, Nums1...>, Poly<N, Nums2...>>::type
+                                          >::type
+                          >::type type;
 };
 
 #endif
