a932d08b33cf38f7235e8f723cd2050ac3baceb1
Anastasia Myshko <me.metida@gmail.com>
2015-04-11 00:29:15 +0300

    static_assert now works, tests for sum and diff metafunctions for ffe added

--- main.cpp
+++ main.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
-//#include "ffe.h"
-//#include "is_prime.h"
 #include "poly.h"
+#include "ffe.h"
 
 using namespace std;
 
 int main(){
-    // Test FFE
-/*	const long N = 7;
-	cout << "N=" << N << endl;
-	FFE<N> *a = new FFE<N>(5);
-	FFE<N> *b = new FFE<N>(4);
-	*a += *b;
-	const FFE<N> *c = FFE<N>::ONE();
 
-    // Test is_prime
-	bool j = is_prime<11>::value;
-*/
+//  Test is_prime
+//  bool j = is_prime_func<11>;
+
+    typedef FFE<7, 5> a;
+    typedef FFE<7, 4> b;
 
     // Test sum
+    typedef FFE<7, 2> ref_res1;
+    typedef FFE_sum<7, a, b>::type res1;
+    static_assert(FFE_equals<7, res1, ref_res1>::value, "Sum for FFE error");
 
+    // Test diff
+    typedef FFE<7, 6> ref_res2;
+    typedef FFE_diff<7, b, a>::type res2;
+    static_assert(FFE_equals<7, res2, ref_res2>::value, "Diff for FFE error");
+    
 
-    typedef sum<3, Poly<3, 2>, Poly<3, 2>>::type s;
-    cout << s::elements[0] << endl;
+    typedef sum<5, Poly<5, 1, 2, 1>, Poly<5, 1, 1, 1>>::type s;
+    for (int i = 0; i < s::elements_count; i++)
+        cout << s::elements[i];
+    cout << endl;
+
+    typedef mult<3, Poly<3, 1, 2, 1>, Poly<3, 1, 1, 1>>::type m;
+    for (int i = 0; i < m::elements_count; i++)
+        cout << m::elements[i];
+    cout << endl;
 
 }
